7779c89e04a6a6f84e399a6badea52b692864c58
jaunger1999 <92285971+jaunger1999@users.noreply.github.com>
2024-10-11 00:31:35 -0500

    fix

--- raytriangleintersection.h
+++ raytriangleintersection.h
@@ -1,7 +1,7 @@
 struct CollisionRay {
 	float ox, oy, oz, ow;
 	float dx, dy, dz, dw;
-} __attribute__((aligned(16)));
+} __declspec(aligned(16));
 
 typedef struct CollisionRay CollisionRay;
 
@@ -9,13 +9,13 @@ struct PrecomputedTriangle {
 	float nx, ny, nz, nd;
 	float ux, uy, uz, ud;
 	float vx, vy, vz, vd;
-} __attribute__((aligned(16)));
+} __declspec(aligned(16));
 
 typedef struct PrecomputedTriangle PrecomputedTriangle;
 
 struct Hit {
 	float px, py, pz, pw;
 	float t, u, v;
-} __attribute__((aligned(16)));
+} __declspec(aligned(16));
 
 typedef struct Hit Hit;
