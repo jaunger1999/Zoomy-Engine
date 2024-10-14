07882b58a0b9cb9c4fbeabd13f8006eac0ecbe0a
jaunger1999 <92285971+jaunger1999@users.noreply.github.com>
2024-10-14 04:48:07 -0500

    starting a test for our collision code.

---
+++ collisionTest.c
@@ -0,0 +1,13 @@
+#include "raylib.h"
+#include "raytriangleintersection.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+int main(void) {
+	Ray ray = { 0 };
+
+	return 0;
+}
