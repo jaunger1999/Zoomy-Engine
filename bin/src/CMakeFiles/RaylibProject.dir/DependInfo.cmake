48750e3c6902c4e171b8193bfce5b2e58bcdfe74
jaunger1999 <92285971+jaunger1999@users.noreply.github.com>
2024-11-23 04:12:43 -0600

    cmake shenanigans

---
+++ bin/src/CMakeFiles/RaylibProject.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jaung/source/repos/Raylib Project/src/d_dict.c" "src/CMakeFiles/RaylibProject.dir/d_dict.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/d_dict.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/d_queue.c" "src/CMakeFiles/RaylibProject.dir/d_queue.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/d_queue.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/f_objparser.c" "src/CMakeFiles/RaylibProject.dir/f_objparser.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/f_objparser.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/g_events.c" "src/CMakeFiles/RaylibProject.dir/g_events.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/g_events.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/hash.c" "src/CMakeFiles/RaylibProject.dir/hash.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/hash.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/m_cylinder.c" "src/CMakeFiles/RaylibProject.dir/m_cylinder.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/m_cylinder.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/m_raytriangleintersection.c" "src/CMakeFiles/RaylibProject.dir/m_raytriangleintersection.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/m_raytriangleintersection.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/m_vector.c" "src/CMakeFiles/RaylibProject.dir/m_vector.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/m_vector.c.obj.d"
+  "C:/Users/jaung/source/repos/Raylib Project/src/main.c" "src/CMakeFiles/RaylibProject.dir/main.c.obj" "gcc" "src/CMakeFiles/RaylibProject.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bin/src/CMakeFiles/RaylibProject.dir/hash.c.obj.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/RaylibProject.dir/hash.c.obj: \
+ C:\Users\jaung\source\repos\Raylib\ Project\src\hash.c
