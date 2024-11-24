9442d5b420ceea279c310cf7cc7b8d30d4c48509
jaunger1999 <92285971+jaunger1999@users.noreply.github.com>
2024-11-23 21:21:50 -0600

    cleanup

--- src/compile t_cdfparser.bat
+++
@@ -1,2 +0,0 @@
-gcc t_cdfparser.c d_dict_s.c d_string.c f_cdfparser.c g_statemachines.c hash.c -o ../bin/t_cdfparser.exe -g3 -O1 -Wall -std=c99 -Wno-missing-braces -msse4.1 -I ../include/ -L ../lib/ -lraylib -llua -lfluidsynth -lpcg_random -lopengl32 -lgdi32 -lwinmm
-cmd /k
